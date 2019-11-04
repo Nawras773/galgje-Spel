86cf9ebe744d707c7891ee73d0ac6f992b058eb1
nawras <nawrasabomahmod@gmail.com>
2019-11-04 12:56:19 +0100

    "basher update"
    git commit -mbasher update

--- src/Built.java
+++ src/Built.java
@@ -23,7 +23,65 @@ public class Built {
             
             for(int i=0; i<playerGuess.length; i++){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
                 playerGuess[i] =  '_';
-            } 
+            }
+            boolean wordIsGuessed = false;
+            int tries = 0;
+
+            while(!wordIsGuessed && tries != ammountOfGuesses){
+                System.out.println("Huidige raden: ");
+                print(playerGuess);
+                System.out.printf("Je hebt %d aantal pogingen over.\n", ammountOfGuesses-tries);
+                System.out.println("Voer een enkel teken in: ");
+                char input = scanner.nextLine().charAt(0);
+                tries++;
+
+                if(input == '-'){
+                    wordIsGuessed = true;
+                    weArePlaying = false;
+                } else{
+                    for(int i=0; i<randomWordToGuess.length; i++){
+                        if(randomWordToGuess[i] == input){
+                            playerGuess[i] = input;
+                        }
+                    }
+
+                    if(isTheWordGuessed(playerGuess)){
+                        wordIsGuessed = true;
+                        System.out.println("Gefeliciteerd");
+                    }
+                }
+            } /* End of wordIsGuessed */
+            if(!wordIsGuessed){
+                System.out.println("Je had geen gissingen meer.");
+            }
+
+            System.out.println("Wil je nog een keer spelen? (ja / nee) ");
+            String choice = scanner.nextLine();
+            if(choice.equals("nee")){
+                weArePlaying = false;
+            }
+
+        }/*End of We Are Playing*/
+
+        System.out.println("Game Over!");
+    }
+
+    public static void print(char array[]){
+        for(int i=0; i<array.length; i++){ // Assign empty dashes at start "_ _ _ _ _ _ _ _"
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static boolean isTheWordGuessed(char[] array){
+        boolean condition = true;
+        for(int i=0; i<array.length; i++){
+            if(array[i] == '_'){
+                condition = false;
+            }
+        }
+        return condition;
+    }
     }
 }}
 
